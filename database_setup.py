e964dcccf9d3af09a4c793add55926a70fe8c23a
mohamedlotfe <mhmdlotfy9@gmail.com>
2017-04-21 23:47:21 +0200

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -41,7 +41,7 @@ class MenuItem(Base):
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    restaurant = relationship(Restaurant,backref=backref("menu_item", cascade="all,delete")
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
